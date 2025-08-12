2237e3da0a01e37981cb89da4dfe360ef63301ce
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-08-12 06:33:56 +0545

    formation

--- Formation/formation.js
+++ Formation/formation.js
@@ -110,7 +110,7 @@ function makeDraggable(element) {
 function saveFormation() {
     const players = [];
     document.querySelectorAll('.player').forEach(player => {
-        players.push({
+        players.push({   
             name: player.querySelector('.player-name').textContent,
             x: (player.offsetLeft / pitch.offsetWidth) * 100,
             y: (player.offsetTop / pitch.offsetHeight) * 100
